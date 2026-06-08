a881c16f2454df9ecbd43f96b2cdb16706cdeec9
Jxsha1 <joshstevens10697@gmail.com>
2026-06-08 16:10:39 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'finance-app-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'Financial Update';
+  const options = {
+    body: data.body || 'You have a new notification regarding your budget.',
+    icon: '/icons/icon-192x192.png',
+    badge: '/icons/icon-192x192.png',
+    data: data.url || '/'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data)
+  );
+});
